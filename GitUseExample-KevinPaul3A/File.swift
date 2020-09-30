ec775e8688524a8b858b7af1ce6871f1f9bf88b9
Kevin Paul <kevin.paul.418@k12.friscoisd.org>
2020-09-30 14:05:18 -0500

    Added new map-for-itinerary feature

---
+++ GitUseExample-KevinPaul3A/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  GitUseExample-KevinPaul3A
+//
+//  Created by Paul, Kevin P on 9/30/20.
+//  Copyright © 2020 Paul, Kevin P. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
